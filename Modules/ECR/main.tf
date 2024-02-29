c1a2a16352b88bdd7e3ff863f3c5d483511c9583
Tatev Hunanyan <124250439+Tatevik2000@users.noreply.github.com>
2024-02-29 22:20:57 +0400

    Update main.tf

--- Modules/ECR/main.tf
+++ Modules/ECR/main.tf
@@ -1,11 +1,18 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-0
-
-/*=========================================
-      AWS Elastic Container Repository
-==========================================*/
-
 resource "aws_ecr_repository" "ecr_repository" {
   name                 = var.name
   image_tag_mutability = "MUTABLE"
+  policy = jsonencode({
+    Version = "2008-10-17",
+    Statement = [{
+      Sid       = "AllowPull"
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = [
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetAuthorizationToken"
+      ]
+    }]
+  })
 }
