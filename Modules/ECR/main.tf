c14f7d6898e3049bb4fb7ef395133e43de4ee433
Tatev Hunanyan <124250439+Tatevik2000@users.noreply.github.com>
2024-02-29 22:50:34 +0400

    Update main.tf

--- Modules/ECR/main.tf
+++ Modules/ECR/main.tf
@@ -1,18 +1,28 @@
-resource "aws_ecr_repository" "ecr_repository" {
-  name                 = var.name
-  image_tag_mutability = "MUTABLE"
-  policy = jsonencode({
-    Version = "2008-10-17",
-    Statement = [{
-      Sid       = "AllowPull"
-      Effect    = "Allow"
-      Principal = "*"
-      Action    = [
-        "ecr:GetDownloadUrlForLayer",
-        "ecr:BatchGetImage",
-        "ecr:BatchCheckLayerAvailability",
-        "ecr:GetAuthorizationToken"
-      ]
-    }]
-  })
-}
+resource "aws_ecr_repository" "ecr_repository" {  
+  name                 = var.name  
+  image_tag_mutability = "MUTABLE"  
+}  
+  
+data "aws_iam_policy_document" "policy" {  
+  statement {  
+    sid    = "PublicRead"  
+    effect = "Allow"  
+  
+    principals {  
+      type        = "*"  
+      identifiers = ["*"]  
+    }  
+  
+    actions = [  
+      "ecr:GetDownloadUrlForLayer",  
+      "ecr:BatchGetImage",  
+      "ecr:BatchCheckLayerAvailability",  
+    ]  
+  }  
+}  
+  
+resource "aws_ecr_repository_policy" "policy" {  
+  repository = aws_ecr_repository.ecr_repository.name  
+  policy     = data.aws_iam_policy_document.policy.json  
+}  
+
