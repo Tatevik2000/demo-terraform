700755567ba087b3b5dd2adbd08b2a4b72827bd8
Tatev Hunanyan <124250439+Tatevik2000@users.noreply.github.com>
2024-03-01 01:28:56 +0400

    Update variables.tf

--- Modules/ECS/Service/variables.tf
+++ Modules/ECS/Service/variables.tf
@@ -1,3 +1,33 @@
+variable "desired_tasks" {  
+  description = "The number of desired tasks to run in the ECS service"  
+  type        = number  
+}  
+  
+variable "ecs_cluster_id" {  
+  description = "The ID of the ECS cluster"  
+  type        = string  
+}  
+  
+variable "arn_task_definition" {  
+  description = "The ARN of the ECS task definition"  
+  type        = string  
+}  
+  
+variable "security_group_ids" {  
+  description = "The list of security group IDs to associate with the ECS service"  
+  type        = list(string)  
+}  
+  
+variable "subnet_ids" {  
+  description = "The list of subnet IDs for the ECS service"  
+  type        = list(string)  
+}  
+  
+variable "target_group_arn" {  
+  description = "The ARN of the target group for the ECS service"  
+  type        = string  
+}  
+
 variable "name" {  
   description = "The name of the ECS task definition"  
   type        = string  
