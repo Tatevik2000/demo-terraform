dcdf2c50d70a2182cbdd41dd7bea06d32a4d8c18
Tatev Hunanyan <124250439+Tatevik2000@users.noreply.github.com>
2024-02-29 23:08:24 +0400

    Update main.tf

--- Modules/ECR/main.tf
+++ Modules/ECR/main.tf
@@ -2,27 +2,30 @@ resource "aws_ecr_repository" "ecr_repository" {
   name                 = var.name  
   image_tag_mutability = "MUTABLE"  
 }  
-  
-data "aws_iam_policy_document" "policy" {  
-  statement {  
-    sid    = "PublicRead"  
-    effect = "Allow"  
-  
-    principals {  
-      type        = "*"  
-      identifiers = ["*"]  
-    }  
-  
-    actions = [  
-      "ecr:GetDownloadUrlForLayer",  
-      "ecr:BatchGetImage",  
-      "ecr:BatchCheckLayerAvailability",  
-    ]  
-  }  
-}  
-  
+
 resource "aws_ecr_repository_policy" "policy" {  
   repository = aws_ecr_repository.ecr_repository.name  
-  policy     = data.aws_iam_policy_document.policy.json  
+  policy     = <<EOF
+  {
+    "Version": "2008-10-17",
+    "Statement": [
+      {
+        "Sid": "adds full ecr access to the demo repository",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:BatchGetImage",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetLifecyclePolicy",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+      }
+    ]
+  }
+  EOF  
 }  
 
