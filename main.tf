fdfb609c7a12bc3f802a705e519e63e0f2a4099c
Tatev Hunanyan <124250439+Tatevik2000@users.noreply.github.com>
2024-03-02 00:26:39 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -112,7 +112,7 @@ module "ecs_role" {
   create_ecs_role    = true
   name               = var.iam_role_name["ecs"]
   name_ecs_task_role = var.iam_role_name["ecs_task_role"]
-  dynamodb_table     = [${module.dynamodb_table.dynamodb_table_arn}]
+  dynamodb_table     = ["${module.dynamodb_table.dynamodb_table_arn}"]
 }
 
 # ------- Creating a IAM Policy for role -------
