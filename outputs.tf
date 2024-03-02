9c472c851c7fbb5cf3305518e116d0273b66ce73
Tatev Hunanyan <124250439+Tatevik2000@users.noreply.github.com>
2024-03-02 12:26:38 +0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,1 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-0
 
-output "application_url" {
-  value       = module.alb.dns_alb
-  description = "Copy this value in your browser in order to access the deployed app"
-}
-
-output "swagger_endpoint" {
-  value       = "${module.alb.dns_alb}/api/docs"
-  description = "Copy this value in your browser in order to access the swagger documentation"
-}
