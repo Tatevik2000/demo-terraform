39dab0e6dde4bf2bd88d3052635582249675743a
Tatev Hunanyan <124250439+Tatevik2000@users.noreply.github.com>
2024-03-02 12:39:58 +0400

    Update outputs.tf

--- Modules/vpc/outputs.tf
+++ Modules/vpc/outputs.tf
@@ -1,7 +1,7 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
-output "aws_vpc" {
+output "aws_vpc_id" {
   value = aws_vpc.aws_vpc.id
 }
 
