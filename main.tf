258360e36e63b0d1ab179520157876a899fa01d7
Tatev Hunanyan <124250439+Tatevik2000@users.noreply.github.com>
2024-03-02 11:45:39 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ module "alb" {
   name           = "${var.environment_name}-alb"  
   subnets        = [module.vpc.public_subnets[0], module.vpc.public_subnets[1]]  
   security_group = module.security_group_alb.sg_id  
-  target_group   = module.target_group_server_blue.arn_tg
+  target_group   = module.target_group_server_blue.arn_tg_1
     
 }
 
