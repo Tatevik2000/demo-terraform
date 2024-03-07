5079c091f8bce59dbd28f445bdaa767c8a3f6d69
Tatev Hunanyan <124250439+Tatevik2000@users.noreply.github.com>
2024-03-08 02:07:45 +0400

    Update outputs.tf

--- Modules/SSM/outputs.tf
+++ Modules/SSM/outputs.tf
@@ -12,3 +12,9 @@ output "ssm_parameter_arn" {
   description = "The ARN of the SSM parameter"
   value       = aws_ssm_parameter.example.arn
 }
+
+output "ssm_parameter_value" {
+  description = "The value of the SSM parameter (sensitive information)"
+  value       = aws_ssm_parameter.example.value
+  sensitive   = true
+}
