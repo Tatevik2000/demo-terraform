797c6743fd332eab61029995257e15c6525d92f2
Tatev Hunanyan <124250439+Tatevik2000@users.noreply.github.com>
2024-03-09 00:30:43 +0400

    Update main.tf

--- Modules/IAM/main.tf
+++ Modules/IAM/main.tf
@@ -127,9 +127,10 @@ resource "aws_iam_policy" "ecs_ssm_access_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_ssm_access_attachment" {
-  count      = length(aws_iam_role.ecs_task_excecution_role) > 0 ? 1 : 0
-  role       = aws_iam_role.ecs_task_excecution_role[0].name
-  policy_arn = aws_iam_policy.ecs_ssm_access_policy.arn
+  count      = length(aws_iam_policy.ecs_ssm_access_policy) > 0 ? 1 : 0
+  role       = aws_iam_role.ecs_task_execution_role[0].name
+  policy_arn = aws_iam_policy.ecs_ssm_access_policy[0].arn
 }
+
 
 
