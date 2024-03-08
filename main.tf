35480978c87fd8699c5f288af67c316bcb9effda
Tatev Hunanyan <124250439+Tatevik2000@users.noreply.github.com>
2024-03-09 00:20:18 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -119,6 +119,7 @@ module "ecs_role_policy" {
   name          = "ecs-ecr-${var.environment_name}"
   create_policy = true
   attach_to     = module.ecs_role.name_role
+  ssm_parameter_arns = [module.ssm_parameter, module.ssm_parameter_alb]
 }
 
 # ------- Creating client ECR Repository to store Docker Images -------
