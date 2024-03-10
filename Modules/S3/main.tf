9bdab20e0f88e3a2de35da1322e7206062181ed9
Tatev Hunanyan <124250439+Tatevik2000@users.noreply.github.com>
2024-03-10 22:31:13 +0400

    Update main.tf

--- Modules/S3/main.tf
+++ Modules/S3/main.tf
@@ -1,10 +1,3 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-0
-
-/*===========================
-      AWS S3 resources
-============================*/
-
 resource "aws_s3_bucket" "s3_bucket" {
   bucket        = var.bucket_name
   acl           = "private"
