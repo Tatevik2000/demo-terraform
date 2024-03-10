14963b3fda43b9eb29cdd58ec773f59e2dd7f17f
Tatev Hunanyan <124250439+Tatevik2000@users.noreply.github.com>
2024-03-10 22:32:23 +0400

    Delete Modules/SNS directory

--- Modules/SNS/variables.tf
+++
@@ -1,7 +0,0 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-0
-
-variable "sns_name" {
-  description = "The name of the SNS topic"
-  type        = string
-}
