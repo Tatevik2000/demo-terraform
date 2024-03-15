7cd9e537de5ddc9e37a7cc392e99394eb8021322
Tatev Hunanyan <124250439+Tatevik2000@users.noreply.github.com>
2024-03-15 12:36:12 +0400

    Update main.tf

--- Modules/vpc/main.tf
+++ Modules/vpc/main.tf
@@ -73,7 +73,6 @@ resource "aws_default_route_table" "rt_public" {
 
 # ------- Create EIP -------
 resource "aws_eip" "eip" {
-  vpc = true
   domain = "vpc"
   tags = {
     Name = "eip-${var.name}"
