d111b64167058b5721ce1125da45978330345e4f
Tatev Hunanyan <124250439+tatev03@users.noreply.github.com>
2024-03-15 20:23:59 +0400

    Update main.tf

--- Modules/IAM/main.tf
+++ Modules/IAM/main.tf
@@ -72,15 +72,6 @@ data "aws_iam_policy_document" "ecs_task_assume_role_policy" {
   
 data "aws_iam_policy_document" "role_policy_ecs_task_role" {  
   statement {  
-    sid       = "AllowS3Actions"  
-    effect    = "Allow"  
-    actions   = [  
-      "s3:GetObject",  
-      "s3:ListBucket"  
-    ]  
-    resources = var.s3_bucket_assets  
-  }  
-  statement {  
     sid       = "AllowIAMPassRole"  
     effect    = "Allow"  
     actions   = [  
