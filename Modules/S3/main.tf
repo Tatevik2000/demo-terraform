ea46a66872dd5c4a768dca7c81a306b484c6ac0b
Tatev Hunanyan <124250439+tatev03@users.noreply.github.com>
2024-03-15 19:49:59 +0400

    Delete Modules/S3 directory

--- Modules/S3/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket        = var.bucket_name
-  force_destroy = true
-  tags = {
-    Name = var.bucket_name
-  }
-}
--- Modules/S3/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "bucket_name" {
-  description = "The name of your S3 bucket"
-  type        = string
-}
